98148f8bd836429a9f63946f48485f0ecfa18524
unknown <wasim_iqbal@msn.com>
2022-10-09 11:49:20 -0700

    Small JS Projects

---
+++ JS_project0/script.js
@@ -0,0 +1,3 @@
+prompt("Hi! How are you?"); //show an alert to the viewer
+
+console.log("Hellow Console from Java Script");
---
+++ JS_project1/script.js
@@ -0,0 +1,3 @@
+const userName = prompt("Hi! What is your Name?"); //show an alert to the viewer
+
+console.log(userName);
